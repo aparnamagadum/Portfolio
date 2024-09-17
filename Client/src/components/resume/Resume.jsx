532b76ed2643b5363bec7afa619a4e7baeffd920
aparnamagadum <aparnamagadum2@gmail.com>
2024-09-17 19:57:45 +0530

    deployed server

--- Client/src/components/resume/Resume.jsx
+++ Client/src/components/resume/Resume.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Resume.css"
-import Button from '../projectSection/button';
+import Button from '../projectSection/Button.jsx';
 import Nav from '../navbar/Nav';
 import Footer from '../footer/Footer';
 function Resume() {
