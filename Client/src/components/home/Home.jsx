e0edb537168df9db9e8657f71a154d25a25bb0c5
aparnamagadum <aparnamagadum2@gmail.com>
2024-09-18 09:11:02 +0530

    added files

--- Client/src/components/home/Home.jsx
+++ Client/src/components/home/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   return (
     <div className='home'>
      <Nav/>
-     <HomeContainer title1="Hi " title2="I'M Aparna Magadum" title3="I am a passionate Full Stack Web Developer, specialized in ( HTML, CSS , Tailwind CSS , MongoDB , Javascript , Express.js , React.js , and Node.js ). With a strong focus on web development." image={Picture}/>
+     <HomeContainer title2="I'M Aparna Magadum" title3="I am a passionate Full Stack Web Developer, specialized in ( HTML, CSS , Tailwind CSS , MongoDB , Javascript , Express.js , React.js , and Node.js ). With a strong focus on web development." image={Picture}/>
     <div className='Social-media'>
       <h1>Find Me On</h1>
        <h2>Feel free to connect with me</h2>
--- Client/src/components/home/HomeContainer.jsx
+++ Client/src/components/home/HomeContainer.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './HomeContainer.css'
-function HomeContainer({title1,title2,title3 ,image}) {
+function HomeContainer({title2,title3 ,image}) {
   return (
     <div className='home-container'>
       <div className='left-container'>
-        <h1>{title1}</h1>
-        <h1>{title2}</h1>
-        <h2>{title3}</h2>
+        <h1><i>{title2}</i></h1>
+        <h2><i>{title3}</i></h2>
       </div>
       <div className='right-container'>
         <img className='img' src={image} alt="" />
